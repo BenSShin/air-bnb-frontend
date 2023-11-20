263808b5152ee842a139b2b485d962eca5652d49
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-11-20 15:17:05 -0500

    adds RoomsIndex

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,26 @@
+import { RoomsIndex } from "./RoomsIndex";
+
 export function Content() {
+  const rooms = [
+    {
+      user_id: 1,
+      address: "1313 Mockingbird Ln",
+      city: "Chicago",
+      state: "Illinois",
+      price: 100.0,
+      description: "Cozy basement apartment",
+      home_type: "apartment building",
+      room_type: "bedroom",
+      total_occupancy: 4,
+      total_bedrooms: 2,
+      total_bathrooms: 1,
+    },
+  ];
+
   return (
     <main>
       <h1>Welcome to React!</h1>
+      <RoomsIndex rooms={rooms} />
     </main>
   );
 }
