171ef98adf609025f1bfff7cee899c714da7adb4
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-11-21 16:36:18 -0500

    adds roomsshow

--- src/Content.jsx
+++ src/Content.jsx
@@ -4,9 +4,12 @@ import { Login } from "./Login";
 import { UserReviews } from "./UserReviews";
 import { Modal } from "./Modal";
 import { useState } from "react";
+import { RoomsShow } from "./RoomsShow";
 
 export function Content() {
   const [isUserReviewsVisible, setIsUserReviewsVisible] = useState(false);
+  const [isRoomsShowVisible, setIsRoomsShowVisible] = useState(false);
+  const [currentRoom, setCurrentRoom] = useState({});
 
   const handleUserReviews = () => {
     setIsUserReviewsVisible(true);
@@ -16,7 +19,16 @@ export function Content() {
     setIsUserReviewsVisible(false);
   };
 
-  const room = [
+  const handleShowClose = () => {
+    setIsRoomsShowVisible(false);
+  };
+
+  const handleRoomsShow = () => {
+    setIsRoomsShowVisible(true);
+    setCurrentRoom(room);
+  };
+
+  const rooms = [
     {
       user_id: 1,
       address: "1313 Mockingbird Ln",
@@ -29,14 +41,18 @@ export function Content() {
       total_occupancy: 4,
       total_bedrooms: 2,
       total_bathrooms: 1,
+      url: "https://remodelerplatform.blob.core.windows.net/wwwmymatrixbasementcom/gallery/original/c4eea334-33ae-49d5-bae3-bc15ba2cab92.jpg",
     },
   ];
 
   return (
     <main>
-      <h1>Welcome to React!</h1>
+      <h1>Welcome to Airbnb!</h1>
       <Login />
-      <RoomsIndex rooms={room} />
+      <RoomsIndex rooms={rooms} onRoomsShow={handleRoomsShow} />
+      <Modal show={isRoomsShowVisible} onClose={handleShowClose}>
+        <RoomsShow room={currentRoom} />
+      </Modal>
       <SignUp />
       <button onClick={handleUserReviews}>Leave a review</button>
       <Modal show={isUserReviewsVisible} onClose={handleClose}>
