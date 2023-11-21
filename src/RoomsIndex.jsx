19eb12161a28fd6858e7d8920e75fd0d0b176b4a
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-11-21 12:34:53 -0500

    adds RoomsIndex code

--- src/RoomsIndex.jsx
+++ src/RoomsIndex.jsx
@@ -5,6 +5,8 @@ export function RoomsIndex(props) {
       {props.rooms.map((room) => (
         <div key={room.room_id}>
           <h2>{room.description}</h2>
+          <h3>{[room.city, room.state]}</h3>
+          <h3>{room.price}</h3>
         </div>
       ))}
     </div>
