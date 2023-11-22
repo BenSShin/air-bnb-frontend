e4ef944db3e0dfe85417b8e5a950bdd31dafbdae
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-11-22 12:49:50 -0500

    adds reserve form to roomshow

--- src/RoomsShow.jsx
+++ src/RoomsShow.jsx
@@ -1,8 +1,9 @@
+/* eslint-disable react/prop-types */
 export function RoomsShow(props) {
   return (
     <div id="rooms-show">
       <h3>{props.room.description}</h3>
-      <img src={props.room.url} />
+      {/* <img src={props.room.url} /> */}
       <p>{props.room.address}</p>
       <p>
         {props.room.city}, {props.room.state}
@@ -13,6 +14,18 @@ export function RoomsShow(props) {
       <p>{props.room.total_bathrooms}</p>
       <p>{props.room.total_occupancy}</p>
       <h3>${props.room.price}</h3>
+      <form>
+        <h3>Reserve</h3>
+        <div>
+          Start Date: <input type="date" />
+        </div>
+        <div>
+          End Date: <input type="date" />
+        </div>
+        <div>
+          <button type="submit">Reserve</button>
+        </div>
+      </form>
     </div>
   );
 }
